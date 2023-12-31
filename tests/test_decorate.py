88e786e7a448b48a5bda4557c21fc5649f70b859
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2023-12-31 23:03:28 +0000

    Add test for exception

--- tests/test_decorate.py
+++ tests/test_decorate.py
@@ -1,7 +1,12 @@
 from pyka import lib
+import pytest
 
 def test_decorate(mocker):
     mocked_func = mocker.patch('pyka.lib.decorate')
     mocked_func.return_value = 'fish'
     assert lib.decorate("AAA") == 'fish'
     mocked_func.assert_called_once_with("AAA")
+
+def test_exception(mocker):
+    with pytest.raises(ZeroDivisionError):
+        1/0
