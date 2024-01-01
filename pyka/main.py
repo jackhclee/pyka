92be53ca59e1bde33270360dad62d5e10530971c
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2024-01-01 23:34:12 +0000

    Added black code formatter

--- pyka/main.py
+++ pyka/main.py
@@ -2,12 +2,14 @@ import markdown
 import requests
 import types
 
+
 def hello_world(txt):
     try:
         r = requests.get("https://localhost/jjj")
     except Exception as e:
-        r = types.SimpleNamespace(**{'text': "This is error"})
-    return markdown.markdown(f'{txt} - {r.text}')
+        r = types.SimpleNamespace(**{"text": "This is error"})
+    return markdown.markdown(f"{txt} - {r.text}")
+
 
 if __name__ == "__main__":
     print("Main")
